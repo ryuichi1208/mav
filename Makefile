87529eb1cbecd94f6f9ed8fb84c4bbd45a02b4a9
watanabe.ryuichi <jpz2046@linecorp.com>
2019-08-30 16:48:41 +0900

    add client server

--- Makefile
+++ Makefile
@@ -2,15 +2,20 @@ NAME     := mav
 VERSION  := v0.0.1
 REVISION := $(shell git rev-parse --short HEAD)
 
+GOBIN=$(shell which go)
 GOVERSION=$(shell go version)
 GOOS=$(shell go env GOOS)
 GOARCH=$(shell go env GOARCH)
 
 SRC=src
 
-.PHONY: run
-run:
-	go run $(SRC)/main.go
+.PHONY: run_srv
+run_srv:
+	$(GOBIN) run $(SRC)/main.go --server
+
+.PHONY: run_cli
+run_cli:
+	$(GOBIN) run $(SRC)/main.go --client
 
 .PHONY: test
 test:
--- src/main.go
+++ src/main.go
@@ -1,7 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+var (
+	serverFlag bool
+	clientFlag bool
+)
+
+func optParse() {
+	flag.BoolVar(&serverFlag, "server", false, "help message \"server\" option")
+	flag.BoolVar(&clientFlag, "client", false, "help message \"client\" option")
+
+	flag.Parse()
+
+	if serverFlag == true && clientFlag == true {
+		fmt.Println("Invalid argument")
+		os.Exit(1)
+	}
+}
+
+func genIpAddr() string {
+	ipaddr := fmt.Sprintf("%s:%d", "0.0.0.0", 8090)
+	return ipaddr
+}
+
+func server() {
+	ipaddr := genIpAddr()
+	listener, err := net.Listen("tcp", ipaddr)
+	if err != nil {
+		fmt.Printf("Listen error: %s\n", err)
+		return
+	}
+	defer listener.Close()
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Printf("Accept error: %s\n", err)
+		return
+	}
+
+	fmt.Println("Recv Message")
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Read error: %s\n", err)
+		}
+		fmt.Print(string(buf[:n]))
+	}
+}
+
+func client() {
+	conn, err := net.Dial("tcp", "localhost:8090")
+	if err != nil {
+		fmt.Printf("Dial error: %s\n", err)
+		return
+	}
+	defer conn.Close()
+
+	sendMsg := "Test Messsage.\n"
+	conn.Write([]byte(sendMsg))
+}
 
 func main() {
-	fmt.Println("vim-go")
+	optParse()
+
+	if serverFlag == true {
+		fmt.Println("Server")
+		server()
+	} else if clientFlag == true {
+		fmt.Println("Client")
+		client()
+	} else {
+		fmt.Println("nothing", serverFlag)
+		os.Exit(1)
+	}
 }
