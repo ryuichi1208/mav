8f6519c5f826ba035fb8845b4c4d52e6909e493a
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-09-19 18:23:46 +0900

    Update mavServ.go

--- src/tmp/mavServ.go
+++ src/tmp/mavServ.go
@@ -1,5 +1,16 @@
 package main
 
+/*
+   ____    __
+  / __/___/ /  ___
+ / _// __/ _ \/ _ \
+/___/\__/_//_/\___/ v4.0.0
+High performance, minimalist Go web framework
+https://echo.labstack.com
+____________________________________O/_______
+                                    O\
+  */
+
 import (
     "log"
     "net/http"
@@ -12,6 +23,48 @@ type postHelloInput struct {
 
 type postHelloOutput struct {
     Result string
+}
+
+/*
+type Error struct {
+	Status     int64  `json:"status"`
+	Type       string `json:"type"`
+	Code   string `json:"code,omitempty"`
+	Errors []struct {
+		Code       string `json:"code,omitempty"`
+		Detail     string `json:"detail,omitempty"`
+		Field      string `json:"field,omitempty"`
+		UserDetail string `json:"user_detail,omitempty"`
+	} `json:"errors,omitempty"`
+	UserDetail string `json:"user_detail,omitempty"`
+}
+ */
+
+type DbConf struct {
+    Dialect  string
+    User     string `env:"MysqlUser"`
+    Password string `env:"MysqlPassword"`
+    Address  string `env:"MysqlAddress"`
+    Port     int    `env:"MysqlPort"`
+    Dbname   string `env:"MysqlDbname"`
+}
+
+/* 
+    dialect:  mysql
+    host:     localhost
+    port:     3306
+    user:     root
+    password:
+    dbname:   golang_api_sample
+ */
+
+func IsTemporary(err error) bool {
+        te, ok := err.(temporary)
+        return ok && te.Temporary()
+}
+
+func (e *MyError) Error() string { 
+        return fmt.Sprintf("%s:%d: %s”, e.File, e.Line, e.Msg)
 }
 
 func postHello(w rest.ResponseWriter, req *rest.Request) {
@@ -32,6 +85,17 @@ func postHello(w rest.ResponseWriter, req *rest.Request) {
     w.WriteJson(&postHelloOutput{
         "Hello, " + input.Name,
     })
+}
+
+/* 
+type mock struct{}
+func (m *mock) GetById(id int64) entity.User {
+    return testData
+}
+ */
+
+func hello(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
 }
 
 func main() {
